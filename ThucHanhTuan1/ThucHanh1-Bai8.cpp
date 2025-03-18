0035a660dd453d994f2c89e1efae043059ea0def
estellra <Charafrisksans024@gmail.com>
2025-03-18 22:11:31 +0700

    Update ThucHanh1-Bai8.cpp

--- ThucHanhTuan1/ThucHanh1-Bai8.cpp
+++ ThucHanhTuan1/ThucHanh1-Bai8.cpp
@@ -6,7 +6,7 @@
 
 using namespace std;
 
-bool ktmachuyenbay(const string& a) {
+bool ktmachuyenbay(const string& a) {//kiem tra xem ma chuyen bay co chua ky tuc dac biet khong
     if (a.length()>5) return 0;
     for (char b:a) 
     {
@@ -15,18 +15,18 @@ bool ktmachuyenbay(const string& a) {
     return 1;
 }
 
-bool ktngay(int ng, int t, int n) {
+bool ktngay(int ng, int t, int n) {//kiem tra xem ngay co hop le khong
     if (n<0||n>2025) return 0;
     int Thang[12]={31,28,31,30,31,30,31,31,30,31,30,31};
     if ((n%4==0&&n%100!=0)||n%400== 0) Thang[1] = 29; 
     return (t>=1&&t<=12&&ng>=1&&ng<=Thang[t-1]);
 }
 
-bool ktthoigian(int h,int m) {
+bool ktthoigian(int h,int m) {//kiem tra thoi gian co hop le khong
     return (h>=0&&h<24&&m>= 0&&m<60);
 }
 
-bool ktdiadiem(const string&a) {
+bool ktdiadiem(const string&a) {//kiem tra xem dia diem co chua ky tu dac biet khong
     if (a.length() > 20) return 0;
     for (char b:a) {
         if (!(isalpha(b)||b==' ')) return 0;  
@@ -34,7 +34,7 @@ bool ktdiadiem(const string&a) {
     return 1;
 }
 
-class ChuyenBay {
+class ChuyenBay {//tao class chuyen bay chua cac thong tin cua chuyen bay cung voi cac getter va setter
 private:
     string maChuyenBay;
     int ngay, thang, nam;
@@ -58,7 +58,7 @@ private:
         void setPhut(int p) { phut = p; }
         void setNoiDi(const string &di) { noidi = di; }
         void setNoiDen(const string &den) { noiden = den; }
-        void output() const {
+        void output() const {// định dạng để lúc xuất nhìn đẹp hơn
             cout << left << setw(8) << maChuyenBay
                 << setw(12) << (to_string(ngay) + "/" + to_string(thang) + "/" + to_string(nam))
                 << setw(8) << (to_string(gio) + ":" + (phut < 10 ? "0" : "") + to_string(phut))
@@ -66,7 +66,7 @@ private:
                 << setw(10) << noiden << endl;
         }
 
-        bool operator<(const ChuyenBay& cb) const {
+        bool operator<(const ChuyenBay& cb) const {//nạp chồng toán tử để so sánh thời gian
             if (nam != cb.nam) return nam < cb.nam;
             if (thang != cb.thang) return thang < cb.thang;
             if (ngay != cb.ngay) return ngay < cb.ngay;
@@ -75,27 +75,28 @@ private:
         }
     };
 
-    class DanhSachChuyenBay {
+    class DanhSachChuyenBay {//class danh sách chuyến bay và các hàm tính toán
     private:
         vector<ChuyenBay> DanhSach;
         int n;
 
     public:
-    void input(DanhSachChuyenBay&);
-    void hienThiDanhSach() const {
+    void input(DanhSachChuyenBay&);//hàm input
+    void hienThiDanhSach() const {//hàm output
         cout<<left<<setw(8)<<"Ma chuyen bay"<<setw(8)<<"  Ngay"<<setw(8)<<"Gio"; 
         cout<<setw(8)<<"Noi di"<<setw(8)<<"Noi den"<<"\n";
         for (const auto& cb:DanhSach) {
             cb.output();
         }
     }
-    void timkiemtheongaydivanoidi(string,int,int,int);
-    void timkiemtheonoidivanoiden(string,string);
+    void timkiemtheongaydivanoidi(string,int,int,int);//tìm kiếm theo nơi đi và ngày đi
+    void timkiemtheonoidivanoiden(string,string);//tìm kiếm theo nơi đi và nơi đên
     void sapXepChuyenBay() {
         sort(DanhSach.begin(), DanhSach.end());
     }
 };
-void DanhSachChuyenBay::input(DanhSachChuyenBay &a) {
+void DanhSachChuyenBay::input(DanhSachChuyenBay &a)//ham nhap vao cac chuyen bay
+ {
     cout << "Nhap vao so luong chuyen bay: ";
     cin >> a.n;
     a.DanhSach.resize(a.n); 
@@ -144,7 +145,8 @@ void DanhSachChuyenBay::input(DanhSachChuyenBay &a) {
         a.DanhSach[i].setNoiDen(den);
     }
 }
-void DanhSachChuyenBay::timkiemtheongaydivanoidi(string khoihanh,int ng,int t,int n) {
+void DanhSachChuyenBay::timkiemtheongaydivanoidi(string khoihanh,int ng,int t,int n) //tim kiem chuyen bay theo ngay di va noi di theo nơi đi và ngày đi cho trước
+{
    
     cout<<"Danh sach cac chuyen bay khoi hanh tu "<<khoihanh<<" vao ngay "<<ng<<"/"<<t<<"/"<<n<<":\n";
 
@@ -160,7 +162,7 @@ void DanhSachChuyenBay::timkiemtheongaydivanoidi(string khoihanh,int ng,int t,in
         cout<<"Khong co chuyen bay nao khoi hanh tu "<<khoihanh<<" vao ngay "<<ng<<"/"<<t<<"/"<<n<<".\n";
     }
 }
-void DanhSachChuyenBay::timkiemtheonoidivanoiden(string khoihanh,string dichden)
+void DanhSachChuyenBay::timkiemtheonoidivanoiden(string khoihanh,string dichden)//tìm kiếm chuyến bay theo nơi đi và nơi đến đã cho trước
 {
     cout<<"Danh sach cac chuyen bay khoi hanh tu "<<khoihanh<<" va den "<<dichden<<":\n";
     bool c=0;
@@ -178,9 +180,9 @@ void DanhSachChuyenBay::timkiemtheonoidivanoiden(string khoihanh,string dichden)
 
 int main() {
     DanhSachChuyenBay ds;
-    ds.input(ds);
-    ds.sapXepChuyenBay();
-    int choice;
+    ds.input(ds);//nhập vào các chuyếnn bay
+    ds.sapXepChuyenBay();//sắp xếp
+    int choice;//lựa chọn chức năng
     cout<<"Nhan 1 de hien thi danh sach toan bo chuyen bay.\n";
     cout<<"Nhan 2 de tim kiem cac chuyen bay co cung noi khoi hanh trong cung mot ngay.\n";
     cout<<"Nhan 3 de tim kiem cac chuyen bay co cung noi di va noi den\n";
@@ -189,7 +191,7 @@ int main() {
     {
         case 1:
         {
-            ds.hienThiDanhSach();
+            ds.hienThiDanhSach();//hiển thị toàn bộ chuyến bay
             break;
         }
         case 2:
@@ -201,7 +203,7 @@ int main() {
             cout<<"Xin nhap vao ngay khoi hanh.\n";
             int ng,t,n;
             cin>>ng>>t>>n;
-            ds.timkiemtheongaydivanoidi(khoihanh,ng,t,n);
+            ds.timkiemtheongaydivanoidi(khoihanh,ng,t,n);//tìm kiếm theo ngày đi và nơi đi
             break;
         }
         case 3:
@@ -212,7 +214,7 @@ int main() {
             getline(cin,khoihanh);
             cout<<"Xin moi nhap vao diem den ma ban mong muon.\n";
             getline(cin,diemden);
-            ds.timkiemtheonoidivanoiden(khoihanh,diemden);
+            ds.timkiemtheonoidivanoiden(khoihanh,diemden);//tìm kiếm theo nơi đi và nơi đến
             break;
         }
     }
